6a5bab5084da0e1f84f43bf55a02c8b913632c88
zqh1209 <3535521945@qq.com>
2024-11-01 17:42:07 +0800

    指标类型

---
+++ instrumentation/Counter/Float.py
@@ -0,0 +1,15 @@
+import random
+from prometheus_client import Counter
+
+REQUESTS = Counter("hello_worlds_total", "Hello Worlds requested.")
+SALES = Counter("hello_world_sales_euro_total", "Euros made serving Hello World.")
+
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        REQUESTS.inc()
+        euros = random.random()
+        SALES.inc(euros)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write("Hello World for {} euros.".format(euros).encode())
---
+++ instrumentation/Histogram/Bucket.py
@@ -0,0 +1,23 @@
+import http.server
+from prometheus_client import start_http_server
+from prometheus_client import Histogram
+
+LATENCY = Histogram(
+    "hello_world_latency_seconds",
+    "Time for a request Hello World.",
+    buckets=[0.0001, 0.0002, 0.0005, 0.001, 0.01, 0.1],
+)
+
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    @LATENCY.time()
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello World")
+
+
+if __name__ == "__main__":
+    start_http_server(8000)
+    server = http.server.HTTPServer(("localhost", 8001), MyHandler)
+    server.serve_forever()
