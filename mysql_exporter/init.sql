3faac0d4f85f414294193e6837cf04a25a3aef22
zqh1209 <3535521945@qq.com>
2024-11-02 15:28:23 +0800

    delete

--- mysql_exporter/init.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'exporter'@'172.31.0.%' IDENTIFIED BY 'exporter';
-GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'172.31.0.%';
-GRANT SELECT ON performance_schema.* TO 'exporter'@'172.31.0.%'
